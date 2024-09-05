1b0589ebf4dc38e13e51ba4adb15e9bc4e7ce31a
Saisamikshaa <ksamiksha768@gmail.com>
2024-09-05 12:19:27 +0530

    Add files via upload
    updated styles , javascript and html code.

--- timer.js
+++ timer.js
@@ -2,33 +2,31 @@ let second = 0;
 let minute = 0;
 let hour = 0;
 
-const updateSecond = () => {
-  if (second < 60) {
-    second = second + 1;
-  } else {
+const updateClock = () => {
+  second += 1;
+  
+  if (second === 60) {
     second = 0;
+    minute += 1;
+    
+    if (minute === 60) {
+      minute = 0;
+      hour += 1;
+      
+      if (hour === 24) {
+        hour = 0;
+      }
+    }
   }
-};
-const updateMinute = () => {
-  if (minute < 60) {
-    minute = minute + 1;
-  } else {
-    minute = 0;
-  }
-};
-const updatehour = () => {
-  if (hour < 24) {
-    hour = hour + 1;
-  } else {
-    hour = 0;
-  }
-};
-const startTimer = () => {
-  setInterval(() => {
-    updateSecond();
-    updateMinute();
-    updatehour();
-  }, 1000);
+  
+  // Format time with leading zeros
+  const formatTime = (value) => value.toString().padStart(2, '0');
+  
+  // Update the HTML
+  document.getElementById('second').textContent = formatTime(second);
+  document.getElementById('minute').textContent = formatTime(minute);
+  document.getElementById('hour').textContent = formatTime(hour);
 };
 
-startTimer();
+// Start the clock
+setInterval(updateClock, 1000);
